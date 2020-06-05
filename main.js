f9a62e5cb3bfef1e423c99a8cec375e9ce7295e0
SevDev <42201808+Sevelar@users.noreply.github.com>
2020-06-05 19:01:07 +0200

    WIP

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const { BrowserWindow, app, Menu } = require("electron");
+const { app, BrowserWindow } = require("electron");
+//const { autoUpdater } = require("electron-updater");
+const path = require('path');
 
-app.on("ready", () => {
-    let window = new BrowserWindow({
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+/*if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+    app.quit();
+}*/
+
+const createWindow = () => {
+    // Create the browser window.
+    let mainWindow = new BrowserWindow({
         width: 1280,
         height: 720,
         frame: false,
@@ -12,51 +20,44 @@ app.on("ready", () => {
         }
     });
 
-    // Load HTML into window
-    window.loadFile("index.html");
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-    // Closing window
-    window.on("closed", () => {
-        window = null;
-    });
-
-    // Open DevTools
+    // Open the DevTools.
     //window.webContents.openDevTools();
 
-    var menu = Menu.buildFromTemplate([
-        {
-            label: 'File',
-            submenu: [
-                {label: 'Open File...'},
-                {label: 'Save'},
-                {label: "Save As..."},
-                {label: "Exit"}
-            ],
-            label: "Edit",
-            submenu: [
-                {label: 'Undo'},
-                {label: "Redo"},
-                {label: "Cut"},
-                {label: "Copy"},
-                {label: "Paste"}
-            ],
-            label: "View",
-            submenu: [
-                {label: 'Code'},
-                {label: "Document"},
-                {label: "Likecord"}
-            ],
-            label: "Help",
-            submenu: [
-                {label: "Markdown Help"},
-                {label: "About"}
-            ]
-        }
-    ])
-})
+    // Check for the update.
+    /*mainWindow.once('ready-to-show', () => {
+        autoUpdater.checkForUpdatesAndNotify();
+    });*/
+};
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
 app.on("window-all-closed", () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+});
+
+/*autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update-available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update-downloaded');
+})*/
